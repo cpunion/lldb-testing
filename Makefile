29bfe049262b378c197af7782d8dd06d60586978
Li Jie <cpunion@gmail.com>
2024-09-30 21:50:50 +0800

    inline/noinline tests

--- Makefile
+++ Makefile
@@ -1,19 +1,35 @@
 CC = clang
 CFLAGS = -g -O1
+LLTARGET = example/test.ll
+INLINE_LLTARGET = example/test_inline.ll
 TARGET = example/test.out
+INLINE_TARGET = example/test_inline.out
 SOURCE = example/test.c
 
-.PHONY: all build test clean
+.PHONY: all build test clean inline
 
-all: build test
+all: build test test_inline
 
-build: $(TARGET)
+
+build: $(TARGET) $(INLINE_TARGET) $(LLTARGET) $(INLINE_LLTARGET)
+
+$(LLTARGET): $(SOURCE)
+	$(CC) $(CFLAGS) -DTEST_INLINE=0 -S -emit-llvm -o $@ $<
+
+$(INLINE_LLTARGET): $(SOURCE)
+	$(CC) $(CFLAGS) -DTEST_INLINE=1 -S -emit-llvm -o $@ $<
 
 $(TARGET): $(SOURCE)
-	$(CC) $(CFLAGS) -o $@ $<
+	$(CC) $(CFLAGS) -DTEST_INLINE=0 -o $@ $<
+
+$(INLINE_TARGET): $(SOURCE)
+	$(CC) $(CFLAGS) -DTEST_INLINE=1 -o $@ $<
 
 test: $(TARGET)
-	./runtest.sh -v $(TARGET) $(SOURCE)
+	./runtest.sh $(if $(VERBOSE),-v) $(TARGET) $(SOURCE)
+
+test_inline: $(INLINE_TARGET)
+	./runtest.sh $(if $(VERBOSE),-v) $(INLINE_TARGET) $(SOURCE)
 
 clean:
-	rm -f $(TARGET)
+	rm -f $(TARGET) $(INLINE_TARGET) $(LLTARGET)
--- example/test.c
+++ example/test.c
@@ -4,6 +4,15 @@
 #include <complex.h>
 #include <string.h>
 
+// Update the INLINE macro definition
+#if defined(TEST_INLINE) && TEST_INLINE == 1
+    #define INLINE __attribute__((always_inline)) static inline
+#elif defined(TEST_INLINE) && TEST_INLINE == 0
+    #define INLINE __attribute__((noinline))
+#else
+    #define INLINE static inline
+#endif
+
 // Base struct
 typedef struct {
     char* name;
@@ -45,21 +54,6 @@ typedef struct struct_with_all_type_fields {
     int pad2;
 } struct_with_all_type_fields;
 
-// Function prototypes
-void func_with_all_type_struct_param(struct_with_all_type_fields s);
-
-// Add this function prototype near the top of the file
-int func_with_all_type_params(
-    int8_t i8, int16_t i16, int32_t i32, int64_t i64, int i,
-    uint8_t u8, uint16_t u16, uint32_t u32, uint64_t u64, unsigned int u,
-    float f32, double f64, bool b,
-    float complex c64, double complex c128,
-    int* slice, int slice_len, int arr[3], e arr2[3],
-    char* s, e e,
-    struct_with_all_type_fields f, struct_with_all_type_fields* pf, int* pi
-    // interface, map, chan, error, and function pointer are omitted as requested
-);
-
 typedef struct {
     int i;
 } tiny_struct;
@@ -88,7 +82,7 @@ typedef struct {
     int r;
 } big_struct;
 
-void func_struct_params(tiny_struct t, small_struct s, mid_struct m, big_struct b) {
+INLINE void func_struct_params(tiny_struct t, small_struct s, mid_struct m, big_struct b) {
     // Expected:
     //   all variables: t s m b global_int global_struct global_struct_ptr
     //   t.i: 1
@@ -146,7 +140,7 @@ void func_struct_params(tiny_struct t, small_struct s, mid_struct m, big_struct 
     printf("done\n");
 }
 
-void func_struct_ptr_params(tiny_struct* t, small_struct* s, mid_struct* m, big_struct* b) {
+INLINE void func_struct_ptr_params(tiny_struct* t, small_struct* s, mid_struct* m, big_struct* b) {
     // Expected:
     //   all variables: t s m b global_int global_struct global_struct_ptr
     //   t->i: 1
@@ -205,11 +199,12 @@ void func_struct_ptr_params(tiny_struct* t, small_struct* s, mid_struct* m, big_
     printf("done\n");
 }
 
-void scope_if(int branch) {
+INLINE void scope_if(int branch) {
     int a = 1;
     // Expected:
     //   all variables: a branch global_int global_struct global_struct_ptr
     //   a: 1
+    printf("ScopeIf %d\n", a);
     if (branch == 1) {
         int b = 2;
         int c = 3;
@@ -237,7 +232,7 @@ void scope_if(int branch) {
     printf("a: %d\n", a);
 }
 
-void scope_for() {
+INLINE void scope_for() {
     int a = 1;
     for (int i = 0; i < 10; i++) {
         switch (i) {
@@ -264,7 +259,7 @@ void scope_for() {
     printf("a: %d\n", a);
 }
 
-void scope_switch(int i) {
+INLINE void scope_switch(int i) {
     int a = 0;
     switch (i) {
         case 1: {
@@ -306,11 +301,163 @@ void scope_switch(int i) {
     printf("a: %d\n", a);
 }
 
+
+// Function definitions
+INLINE void func_with_all_type_struct_param(struct_with_all_type_fields s) {
+    printf("%p\n", (void*)&s);
+    // Expected:
+    //   all variables: s global_int global_struct global_struct_ptr
+    //   s.i8: '\x01'
+    //   s.i16: 2
+    //   s.i32: 3
+    //   s.i64: 4
+    //   s.i: 5
+    //   s.u8: '\x06'
+    //   s.u16: 7
+    //   s.u32: 8
+    //   s.u64: 9
+    //   s.u: 10
+    //   s.f32: 11
+    //   s.f64: 12
+    //   s.b: true
+    //   s.c64: 13 + 14i
+    //   s.c128: 15 + 16i
+    //   s.slice[0]: 21
+    //   s.slice[1]: 22
+    //   s.slice[2]: 23
+    //   s.arr[0]: 24
+    //   s.arr[1]: 25
+    //   s.arr[2]: 26
+    //   s.arr2[0].i: 27
+    //   s.arr2[1].i: 28
+    //   s.arr2[2].i: 29
+    //   s.s[0]: 'h'
+    //   s.s[1]: 'e'
+    //   s.s[2]: 'l'
+    //   s.s[3]: 'l'
+    //   s.s[4]: 'o'
+    //   s.e.i: 30
+    //   s.pad1: 100
+    //   s.pad2: 200
+    s.i8 = '\b';
+    // Expected:
+    //   s.i8: '\b'
+    //   s.i16: 2
+    printf("%d %d\n", (int)strlen(s.s), s.i8);
+}
+
+// Add this function definition near the bottom of the file
+INLINE int func_with_all_type_params(
+    int8_t i8, int16_t i16, int32_t i32, int64_t i64, int i,
+    uint8_t u8, uint16_t u16, uint32_t u32, uint64_t u64, unsigned int u,
+    float f32, double f64, bool b,
+    float complex c64, double complex c128,
+    int* slice, int arr[3], e arr2[3],
+    char* s, e e,
+    struct_with_all_type_fields f, struct_with_all_type_fields* pf, int* pi
+    // interface, map, chan, error, and function pointer are omitted as requested
+) {
+    // Expected:
+    //   all variables: i8 i16 i32 i64 i u8 u16 u32 u64 u f32 f64 b c64 c128 slice arr arr2 e f global_int global_struct global_struct_ptr pf pi s
+    //   i32: 3
+    //   i64: 4
+    //   i: 5
+    //   u32: 8
+    //   u64: 9
+    //   u: 10
+    //   f32: 11
+    //   f64: 12
+    //   slice[0]: 21
+    //   slice[1]: 22
+    //   slice[2]: 23
+    //   arr[0]: 24
+    //   arr[1]: 25
+    //   arr[2]: 26
+    //   arr2[0].i: 27
+    //   arr2[1].i: 28
+    //   arr2[2].i: 29
+    //   e.i: 30
+    //   i8: '\x12'
+    //   i16: 2
+    //   u8: '\x06'
+    //   u16: 7
+    //   b: true
+    printf("%d %d %d %lld %d %u %u %u %llu %u %f %f %d %f %f %p %d %d %d %s %d %p %p %p\n",
+           i8, i16, i32, i64, i, u8, u16, u32, u64, u, f32, f64, b,
+           crealf(c64), crealf(c128),
+           (void*)slice, arr[0], arr2[0].i, e.i,
+           s, f.i, (void*)pf, (void*)pi, (void*)&f);  // Added (void*)&f as the last argument
+
+    i8 = 9;
+    i16 = 10;
+    i32 = 11;
+    i64 = 12;
+    i = 13;
+    u8 = 14;
+    u16 = 15;
+    u32 = 16;
+    u64 = 17;
+    u = 18;
+    f32 = 19;
+    f64 = 20;
+    b = false;
+    c64 = 21 + 22 * I;
+    c128 = 23 + 24 * I;
+    slice[0] = 31; slice[1] = 32; slice[2] = 33;
+    arr[0] = 34; arr[1] = 35; arr[2] = 36;
+    arr2[0].i = 37; arr2[1].i = 38; arr2[2].i = 39;
+    s = "world";
+    e.i = 40;
+
+    printf("%d %d %d %lld %d %u %u %u %llu %u %f %f %d %f %f %d %d %d %d %d %d %s %d %p %p %p\n",
+           i8, i16, i32, i64, i, u8, u16, u32, u64, u, f32, f64, b,
+           crealf(c64), crealf(c128),
+           slice[0], slice[1], slice[2], arr[0], arr[1], arr[2],
+           s, e.i, (void*)&f, (void*)pf, (void*)pi);
+
+    // Expected:
+    //   i8: '\t'
+    //   i16: 10
+    //   i32: 11
+    //   i64: 12
+    //   i: 13
+    //   u8: '\x0e'
+    //   u16: 15
+    //   u32: 16
+    //   u64: 17
+    //   u: 18
+    //   f32: 19
+    //   f64: 20
+    //   b: false
+    //   c64: 21 + 22i
+    //   c128: 23 + 24i
+    //   slice[0]: 31
+    //   slice[1]: 32
+    //   slice[2]: 33
+    //   arr[0]: 34
+    //   arr[1]: 35
+    //   arr[2]: 36
+    //   arr2[0].i: 37
+    //   arr2[1].i: 38
+    //   arr2[2].i: 39
+    //   s[0]: 'w'
+    //   s[1]: 'o'
+    //   s[2]: 'r'
+    //   s[3]: 'l'
+    //   s[4]: 'd'
+    //   e.i: 40
+
+    // Expected(skip):
+    //   arr: [34, 35, 36]
+    printf("FuncWithAllTypeParams %p\n", (void*)&f);
+    return 1;
+}
+
 int global_int = 301;
 struct_with_all_type_fields global_struct;
 struct_with_all_type_fields* global_struct_ptr;
 
-void Test() {
+INLINE void Test() {
     func_struct_params((tiny_struct){1}, (small_struct){2, 3}, (mid_struct){4, 5, 6}, 
                        (big_struct){7, 8, 9, 10, 11, 12, 13, 14, 15, 16});
     
@@ -390,15 +537,6 @@ void Test() {
     func_with_all_type_struct_param(s_all);
     printf("called function with struct\n");
 
-    func_with_all_type_params(s_all.i8, s_all.i16, s_all.i32, s_all.i64,
-                              s_all.i, s_all.u8, s_all.u16, s_all.u32,
-                              s_all.u64, s_all.u,
-                              s_all.f32, s_all.f64, s_all.b,
-                              s_all.c64, s_all.c128,
-                              s_all.slice, s_all.slice_len, s_all.arr, s_all.arr2,
-                              s_all.s, s_all.e,
-                              s_all, global_struct_ptr, &i);
-
     scope_if(1);
     scope_if(0);
     scope_for();
@@ -415,6 +553,15 @@ void Test() {
 
     // Expected(skip):
     //   global_struct.i8: '\x01'
+
+    func_with_all_type_params(s_all.i8, s_all.i16, s_all.i32, s_all.i64,
+                              s_all.i, s_all.u8, s_all.u16, s_all.u32,
+                              s_all.u64, s_all.u,
+                              s_all.f32, s_all.f64, s_all.b,
+                              s_all.c64, s_all.c128,
+                              s_all.slice, s_all.arr, s_all.arr2,
+                              s_all.s, s_all.e,
+                              s_all, global_struct_ptr, &i);
     printf("%d\n", (*global_struct_ptr).i8);
     printf("done\n");
     printf("\n");
@@ -425,139 +572,4 @@ void Test() {
 int main() {
     Test();
     return 0;
-}
-
-// Function definitions
-void func_with_all_type_struct_param(struct_with_all_type_fields s) {
-    printf("%p\n", (void*)&s);
-    // Expected:
-    //   all variables: s global_int global_struct global_struct_ptr
-    //   s.i8: '\x01'
-    //   s.i16: 2
-    //   s.i32: 3
-    //   s.i64: 4
-    //   s.i: 5
-    //   s.u8: '\x06'
-    //   s.u16: 7
-    //   s.u32: 8
-    //   s.u64: 9
-    //   s.u: 10
-    //   s.f32: 11
-    //   s.f64: 12
-    //   s.b: true
-    //   s.c64: 13 + 14i
-    //   s.c128: 15 + 16i
-    //   s.slice[0]: 21
-    //   s.slice[1]: 22
-    //   s.slice[2]: 23
-    //   s.arr[0]: 24
-    //   s.arr[1]: 25
-    //   s.arr[2]: 26
-    //   s.arr2[0].i: 27
-    //   s.arr2[1].i: 28
-    //   s.arr2[2].i: 29
-    //   s.s[0]: 'h'
-    //   s.s[1]: 'e'
-    //   s.s[2]: 'l'
-    //   s.s[3]: 'l'
-    //   s.s[4]: 'o'
-    //   s.e.i: 30
-    //   s.pad1: 100
-    //   s.pad2: 200
-    s.i8 = '\b';
-    // Expected:
-    //   s.i8: '\b'
-    //   s.i16: 2
-    printf("%d %d\n", (int)strlen(s.s), s.i8);
-}
-
-// Add this function definition near the bottom of the file
-int func_with_all_type_params(
-    int8_t i8, int16_t i16, int32_t i32, int64_t i64, int i,
-    uint8_t u8, uint16_t u16, uint32_t u32, uint64_t u64, unsigned int u,
-    float f32, double f64, bool b,
-    float complex c64, double complex c128,
-    int* slice, int slice_len, int arr[3], e arr2[3],
-    char* s, e e,
-    struct_with_all_type_fields f, struct_with_all_type_fields* pf, int* pi
-    // interface, map, chan, error, and function pointer are omitted as requested
-) {
-    // Expected:
-    //   all variables: i8 i16 i32 i64 i u8 u16 u32 u64 u f32 f64 b c64 c128 slice arr arr2 s e f pf pi
-    //   i32: 3
-    //   i64: 4
-    //   i: 5
-    //   u32: 8
-    //   u64: 9
-    //   u: 10
-    //   f32: 11
-    //   f64: 12
-    //   slice: [21, 22, 23]
-    //   arr: [24, 25, 26]
-    //   arr2: [{27}, {28}, {29}]
-    //   e: {30}
-
-    // Expected(skip):
-    //   i8: '\b'
-    //   i16: 2
-    //   u8: '\x06'
-    //   u16: 7
-    //   b: true
-    printf("%d %d %d %lld %d %u %u %u %llu %u %f %f %d %f %f %p %d %d %d %d %s %d %p %p %p\n",
-           i8, i16, i32, i64, i, u8, u16, u32, u64, u, f32, f64, b,
-           crealf(c64), crealf(c128),
-           (void*)slice, slice_len, arr[0], arr2[0].i, e.i,
-           s, f.i, (void*)pf, (void*)pi, (void*)&f);  // Added (void*)&f as the last argument
-
-    i8 = 9;
-    i16 = 10;
-    i32 = 11;
-    i64 = 12;
-    i = 13;
-    u8 = 14;
-    u16 = 15;
-    u32 = 16;
-    u64 = 17;
-    u = 18;
-    f32 = 19;
-    f64 = 20;
-    b = false;
-    c64 = 21 + 22 * I;
-    c128 = 23 + 24 * I;
-    slice[0] = 31; slice[1] = 32; slice[2] = 33;
-    arr[0] = 34; arr[1] = 35; arr[2] = 36;
-    arr2[0].i = 37; arr2[1].i = 38; arr2[2].i = 39;
-    strcpy(s, "world");
-    e.i = 40;
-
-    printf("%d %d %d %lld %d %u %u %u %llu %u %f %f %d %f %f %d %d %d %d %d %d %s %d %p %p %p\n",
-           i8, i16, i32, i64, i, u8, u16, u32, u64, u, f32, f64, b,
-           crealf(c64), crealf(c128),
-           slice[0], slice[1], slice[2], arr[0], arr[1], arr[2],
-           s, e.i, (void*)&f, (void*)pf, (void*)pi);
-
-    // Expected:
-    //   i8: '\t'
-    //   i16: 10
-    //   i32: 11
-    //   i64: 12
-    //   i: 13
-    //   u8: '\x0e'
-    //   u16: 15
-    //   u32: 16
-    //   u64: 17
-    //   u: 18
-    //   f32: 19
-    //   f64: 20
-    //   b: false
-    //   c64: 21 + 22i
-    //   c128: 23 + 24i
-    //   slice: [31, 32, 33]
-    //   arr2: [{37}, {38}, {39}]
-    //   s: "world"
-    //   e: {40}
-
-    // Expected(skip):
-    //   arr: [34, 35, 36]
-    return 1;
 }
